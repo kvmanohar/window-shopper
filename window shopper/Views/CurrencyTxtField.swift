eb7e7747b3ee6a7de46102f67a76606b478928f6
Manohar Kurapati <kvmanohar@gmail.com>
2017-10-28 20:17:48 +0100

    Created a custom view to display $ symbol in the Text view.

--- window shopper/Views/CurrencyTxtField.swift
+++ window shopper/Views/CurrencyTxtField.swift
@@ -11,8 +11,30 @@ import UIKit
 @IBDesignable
 class CurrencyTxtField: UITextField {
 
+    //use this funciton to see the UI changes in the Interface builder screen.
     override func prepareForInterfaceBuilder() {
         customizeView()
+    }
+    
+    //never call this function if it is empty. This will create performance issues
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2,
+                                                width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.89227477, green: 0.89227477, blue: 0.89227477, alpha: 0.2035798373)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        
+        //Always code clipToBounds = true when creating custom view for the change to take effect.
+        currencyLbl.clipsToBounds = true
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        
+        addSubview(currencyLbl)
     }
     
     override func awakeFromNib() {
@@ -25,6 +47,9 @@ class CurrencyTxtField: UITextField {
         layer.cornerRadius = 5.0
         textAlignment = .center
         textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        
+        //Always code clipToBounds = true when creating custom view for the change to take effect.
+        clipsToBounds = true
         
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
